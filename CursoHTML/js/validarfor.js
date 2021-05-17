339faaf118d56878605f840e48299d156d99cde1
danielmonteros79 <danielmonteros79@gmail.com>
2021-05-17 20:14:24 -0300

    cursohml
    cursohtml

---
+++ CursoHTML/js/validarfor.js
@@ -0,0 +1,24 @@
+<script languaje="javascript">
+function validarfor(){
+
+/*
+var correo  = document.getElementById("mail").value; 
+var nombre  = document.getElementsById("name")[0].value;
+var mensaje = document.getElementsById("msg")[0].value;
+
+var expr = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+
+if ( !expr.test(correo) ){                                                            //COMPRUEBA MAIL
+    alert("Error: La dirección de correo " + correo + " es incorrecta.");
+    return false;
+}
+
+if ((correo == "") || (nombre == "") || (mensaje == "") ) {  //COMPRUEBA CAMPOS VACIOS
+    alert("Los campos no pueden quedar vacios");
+    return true;
+}
+*/
+alert("todo ok");
+}
+</script>
